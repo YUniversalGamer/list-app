bb57f257b1c54b1d98fa14d3a2a4b66eac6c70e4
Jackson Cooper <cooper6517@gmail.com>
2023-05-05 20:15:52 -0500

    Added printList func

--- listApp.py
+++ listApp.py
@@ -1,7 +1,13 @@
-tasks = {
+itemList = {
     1: 'Groceries',
     2: 'Pringles',
     3: 'IDFK This is a test'
     }
+def printList():
+    for x in itemList:
+        print(str(x) + '.' + str(itemList[x]))
+    return 'Done'
+#if userInput ==
 
-print(tasks[1])
+#print(itemList.items())
+printList()
